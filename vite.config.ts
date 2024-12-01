2eb6cc1eedb10f6af94b1a140dcdce0cc88b48b0
Maisa Preis <maisapierinipreis@gmail.com>
2024-12-01 15:29:35 -0300

    build: Trying to fix deploy.

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/equipment-locator',
+  base: '/equipment-locator/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
